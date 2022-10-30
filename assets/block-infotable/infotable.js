961c61f6f49edd778346a4c1a5d963015b6b17c6
wavelet-noise <ishellstrike@gmail.com>
2022-10-30 18:50:00 +0200

    infotable test

---
+++ assets/block-infotable/infotable.js
@@ -0,0 +1,47 @@
+// @ts-check
+
+/**
+ * @param {string} tag
+ * @param {{ [attr: string]: string; }} attrs
+ * @param {any[]} children
+ */
+function e(tag, attrs, ...children) {
+  const element = document.createElement(tag);
+  for (const [attr, value] of Object.entries(attrs)) {
+    element.setAttribute(attr, value);
+  }
+  for (const child of children) {
+    typeof child === "string" || typeof child === "number"
+      ? element.appendChild(document.createTextNode(String(child)))
+      : element.appendChild(child);
+  }
+  return element;
+}
+
+/**
+ * @param {string | number | HTMLElement} cardName text in card header
+ * @param {string} imageSrc link to image
+ * @param {(string | number)[][]} descriptionParts info for table
+ */
+function createCard(cardName, imageSrc, descriptionParts) {
+  return e("div", { class: "card" },
+    e("div", { class: "card__header" }, cardName),
+    e("div", {},
+      e("img", { src: imageSrc, class: "card__img" }),
+    ),
+    e("div", { class: "table" }, ...descriptionParts.map(([partName, _, value]) => {
+      const tableRow = [String(partName), value].filter(Boolean).map(text => e("span", {}, text));
+      return e("div", { class: "table__row" }, ...tableRow)
+    }))
+  );
+}
+
+const card = createCard("Aluminium Boiler", "https://picsum.photos/id/100/200/200", [
+  ["Boiler", "description_machines"],
+  ["HeatInput", "common"],
+  ["FluidInput", "common"],
+  ["FluidOutput", "common"],
+  ["power_output", "common", 4000],
+]);
+
+document.body.appendChild(card);
--- wiki/heat.md
+++ wiki/heat.md
@@ -1,5 +1,12 @@
+<script defer src="./assets/block-infotable/infotable.js"></script>
+<link rel="stylesheet" href="./assets/block-infotable/style.css">
+
 ## Heat
+
+<body>
+</body>
 
 Heat is a form of energy in Evospace. It can be transfered by [heat pipes](heat_pipe.md) and stored in them too
 
 Heat is consuming by some [crafting machines](crafting_machines.md) and [converters](converters.md)
+
